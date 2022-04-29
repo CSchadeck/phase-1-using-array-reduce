3237897a17d2d0a2441dabaf8b77f748f3bcbeb5
Corey Schadeck <corey@schadeck.com>
2022-04-29 16:23:45 -0400

    commit tree 37c39ea9386450dce607fd6e795b39a2d5ddfb77

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+function totalBatteries(arr, reducer, number) {
+  let accum = number;
+  for (const element of arr) {
+      accum = reducer(accum, element);
+  };
+  return accum;
+}
+
+//unction couponCounter(totalAmount, location) {
+//return totalAmount + location.amount;
+//}
+
+//console.log(ourReduce(batteryBatches, couponCounter, 0));
